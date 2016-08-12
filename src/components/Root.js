5c3b671c27605a35a0edb0d56ef69accbbd1220e
Casey Rieder <casey.rieder@gmail.com>
2016-08-12 11:56:30 -0400

    initialized react router

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { Provider } from 'react-redux';
+// import react router (browserHistory ensures clean routes/urls)
+import { Router, Route, browserHistory } form 'react-router';
 import App from './App';
 
 // refactored
 const Root = ({ store }) -> (
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App} />
+    </Router>
   </Provider>
 );
+
+// enable PropTypes validation
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+};
 
 export default Root;
