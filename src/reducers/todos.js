785f85833489c6d388639619c08b41ffe2b5cda4
Casey Rieder <casey.rieder@gmail.com>
2016-08-12 18:08:40 -0400

    normalized & refactored stae shape & todos reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,55 +1,54 @@
-const todo = (state, action) => {
+//import a couple useful things here
+import { combineReducers } from 'redux';
+import todo from './todo';
+// rename todo=> byId, state = {}
+// ADD_TODO & TOGGLE_TODO logic now the same==> because it will call the 'todo' reducer to figure out the difference
+const byId = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false,
-      };
     case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
       return {
         ...state,
-        completed: !state.completed,
+        [action.id]: todo(state[action.id], action),
       };
     default:
       return state;
   }
 };
+// rename todos => allIds
+// will also call the 'todo' reducer file to figure out how to respond
+// ./state of THIS reducer is an array of IDs, not an arrayof todos
 
-const todos = (state = [], action) => {
+const allIds = (state = [], action) => {
   switch (action.type) {
+      // just add the new todo.id to the array of ids
     case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action),
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(t =>
-        todo(t, action)
-      );
+      return [...state, action.id];
     default:
       return state;
   }
 };
+// combine the byId & allIds reducers
+const todos = combineReducers({
+  byId,
+  allIds,
+});
 
 export default todos;
 
+// Handle new shit, return all todos in the current state
+const getAllTodos = (state) =>
+  state.allIds.map(id => state.byId[id]);
 
-// add getVisibleTodos from VisibleTodoList component & make it a named export
-/* CONVETION: default export = reducer fxn; export with 'get' as prefix prepares data to be displayed by UI (a selector fxn that selects things from the UI)*/
-
-// getVisibleTodos will respond to the state of todos (as defined above)
 export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state);
   switch (filter) {
     case 'all':
-      return todos;
+      return allTodos;
     case 'completed':
-      return todos.filter(t => t.completed);
+      return allTodos.filter(t => t.completed);
     case 'active':
-      return todos.filter(t => !t.completed);
+      return allTodos.filter(t => !t.completed);
     default:
       throw new Error(`Unknown filter: ${filter}`);
   }
