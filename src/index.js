1cea421ec8dee30e31436538559a04f092c3b0ef
Casey Rieder <casey.rieder@gmail.com>
2016-08-12 11:17:53 -0400

    enabled persistent state in src/index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,21 @@ import { createStore } from 'redux'
 import reducers from './reducers'
 import App from './components/App';
 
-const store = createStore(reducers);
+// create persistent state to override initialStae values defined in reducers
+const persistedState = {
+  todos: [{
+    id: '0',
+    text: 'Welcome back',
+    completed: false,
+  }],
+};
+// create Store with persisted state
+const store = createStore(
+  reducers,
+  persistedState
+);
+// log store once its created
+console.log(store.getState());
 
 render(
   <Provider store={store}>
