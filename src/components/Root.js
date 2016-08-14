96bc55905bbcbdda062c0efcfe7d1ed5f19816ae
Casey Rieder <casey.rieder@gmail.com>
2016-08-14 18:54:29 -0400

    fixed typos in Root & changed react-router version#

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,11 @@
 import React, { PropTypes } from 'react'
 import { Provider } from 'react-redux';
 // import react router (browserHistory ensures clean routes/urls)
-import { Router, Route, browserHistory } form 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 
 // refactored
-const Root = ({ store }) -> (
+const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={browserHistory}>
       {/* add "{:filter}" to allow urls to match the filter, but the () mean that it is optional */}
