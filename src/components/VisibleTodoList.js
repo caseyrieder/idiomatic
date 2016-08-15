612daea8570ad71741b988f3cb67d279a639ccf1
Casey Rieder <casey.rieder@gmail.com>
2016-08-15 08:41:41 -0400

    Rerendering component on route change--renrender VisibleTodosList based on the clicking of a button

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,16 +1,48 @@
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 // going to read params individually now
 import { withRouter } from 'react-router'; //withRouter inject reoute-relat4ed props (like params)
-import { toggleTodo } from '../actions'
-import TodoList from './TodoList'
+import { toggleTodo } from '../actions';
+import TodoList from './TodoList';
 // import getVisibleTodos from rootReducer
-import { getVisibleTodos } from '../reducers'
+import { getVisibleTodos } from '../reducers';
+// fetch todos from api here
+import { fetchTodos } from '../api';
+/* currently, fetchTodos is inside the componentDidMount lifecycle group (takes place after mounting). BUT, since we can't override the lifecycle fxns form generated components (i.e., TodoList) we need to create a new React Component to handle the fetchTodos call ---> */
+import React, { Component } from 'react';
 
-// move getVisibleTodos into todos reducer
+// so, define the VisibleTodoList component
+// THE ONLY PURPOSE FOR ADDING THIS COMPONENT IS TO ADD THE LIFECYCLE GROUPS
+class VisibleTodoList extends Component {
+  // now, define componentDidMount lifecycle group to handle fetchTodos call to grab todos for current filter & log them (upon the Promise resolving)
+  componentDidMount(){
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos)
+    );
+  }
+  // since componentDidMount only runs once (upon mounting)  clicking on the filters doesn't yet change the actual filter (as we can see in the console). So...we add componentDidUpdate to rerender the component when each filter button is clicked
+  componentDidUpdate(prevProps) {
+    // if prev<> current filter, rerun fetchTodos & log
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos =>
+        console.log(this.props.filter, todos)
+      );
+    }
+  }
+  // define render method to render TodoList, as before
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
 
-const mapStateToProps = (state, { params }) => ({
-  todos: getVisibleTodos(state, params.filter || 'all'),
-});
+// make current filter available as a prop (for componentDidMount...fetchTodos)--
+// calculate filter from params & pass both filter & todos into props as part of the state object
+const mapStateToProps = (state, { params }) => {
+  const filter = params.filter || 'all';
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter,
+  }
+};
 // // if ( prop(args) === action(args) )...simplify
 // const mapDispatchToProps = (dispatch) => ({
 //   onTodoClick(id) {
@@ -20,9 +52,10 @@ const mapStateToProps = (state, { params }) => ({
 
 // withRouter will inject route params (filter prop) into VisibleTodoList
 // simplify mapDispatchToProps b/c args === args
-const VisibleTodoList = withRouter(connect(
+// NO NEED FOR const B/C DEFINED AS CLASS. RENAME TO (VisibleTodoList) because now THAT will have the props that the Component will pass down to TodoList
+VisibleTodoList = withRouter(connect(
   mapStateToProps,
   { onTodoClick: toggleTodo }
-)(TodoList));
+)(VisibleTodoList));
 
 export default VisibleTodoList;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import React from 'react';
 import { render } from 'react-dom'
 import Root from './components/Root';
 import configureStore from './configureStore'; // refactored
+
+/*
 import { fetchTodos } from './api'; // import the fake backend
 
-// use fake backend to seed & log data
-// // this should ensure that the fetchTodos promise resolves to an array of todos
-fetchTodos('all').then(todos =>
-	console.log(todos)
-);
+Remove fake backend here, b/c we wanna fetchTodos from inside the component */
+// // use fake backend to seed & log data
+// // // this should ensure that the fetchTodos promise resolves to an array of todos
+// fetchTodos('all').then(todos =>
+// 	console.log(todos)
+// );
 
 // run the configureStore fxn to create it
 const store = configureStore();
