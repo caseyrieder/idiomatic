1d6ae60e096e35da148730d852bd76b1b54ffeaa
Casey Rieder <casey.rieder@gmail.com>
2016-08-15 09:04:21 -0400

    component-ized VisibleTodoList & made it dispatch actions upon mount/update...added receiveTodos action...destructured props to ensure that toggleTodo can be defined as the action for onTodoClick()

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,14 @@
 import { v4 } from 'node-uuid';
 // let nextTodo = 0;
 
+// add receiveTodos that will pull todos from api to be displayed in VisibleTodoList (or wherever)
+// accepts server response & filter as args & returns an object of type: RECEIVE_TODOS, filter & response
+export const receiveTodos = (filter, response) => ({
+  type: 'RECEIVE_TODOS',
+  filter,
+  response,
+});
+
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
   id: v4(),
--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,7 +1,9 @@
 import { connect } from 'react-redux';
 // going to read params individually now
 import { withRouter } from 'react-router'; //withRouter inject reoute-relat4ed props (like params)
-import { toggleTodo } from '../actions';
+// import receiveTodos to enable action-creation
+// // reduce boilerplate
+import * as actions from '../actions';
 import TodoList from './TodoList';
 // import getVisibleTodos from rootReducer
 import { getVisibleTodos } from '../reducers';
@@ -15,22 +17,37 @@ import React, { Component } from 'react';
 class VisibleTodoList extends Component {
   // now, define componentDidMount lifecycle group to handle fetchTodos call to grab todos for current filter & log them (upon the Promise resolving)
   componentDidMount(){
-    fetchTodos(this.props.filter).then(todos =>
-      console.log(this.props.filter, todos)
-    );
+    this.fetchData();
   }
   // since componentDidMount only runs once (upon mounting)  clicking on the filters doesn't yet change the actual filter (as we can see in the console). So...we add componentDidUpdate to rerender the component when each filter button is clicked
+  // if prev<> current filter, rerun fetchTodos & log
   componentDidUpdate(prevProps) {
-    // if prev<> current filter, rerun fetchTodos & log
     if (this.props.filter !== prevProps.filter) {
-      fetchTodos(this.props.filter).then(todos =>
-        console.log(this.props.filter, todos)
-      );
+      this.fetchData();
     }
   }
+
+  // combine common componentDidMount/Update code
+  /* we want fetchTodos to become part of redux store state, and the ONLY way to get something into the state is to dispatch an action */
+    // this will pass the filter through the action creator (these props are available via mapState & mapDispatch...)
+  fetchData() {
+    const { filter, receiveTodos } = this.props;
+    fetchTodos(filter).then(todos =>
+      receiveTodos(filter, todos)
+    );
+  }
   // define render method to render TodoList, as before
+
   render() {
-    return <TodoList {...this.props} />;
+    // destructure props b/c toggleTodo needs to be passed by onTodoClick callback prop name, but the rest can be as they are
+    const { toggleTodo, ...rest } = this.props;
+    // pass toggleTodo as action for onTodoClick prop, but the other props as themselves
+    return (
+      <TodoList
+        {...rest}
+        onTodoClick={toggleTodo}
+      />
+    );
   }
 }
 
@@ -41,7 +58,7 @@ const mapStateToProps = (state, { params }) => {
   return {
     todos: getVisibleTodos(state, filter),
     filter,
-  }
+  };
 };
 // // if ( prop(args) === action(args) )...simplify
 // const mapDispatchToProps = (dispatch) => ({
@@ -53,9 +70,11 @@ const mapStateToProps = (state, { params }) => {
 // withRouter will inject route params (filter prop) into VisibleTodoList
 // simplify mapDispatchToProps b/c args === args
 // NO NEED FOR const B/C DEFINED AS CLASS. RENAME TO (VisibleTodoList) because now THAT will have the props that the Component will pass down to TodoList
+// // add receiveTodos as an action to the mapDispatchToProps so that this component can dispatch the receiveTodos action
+// reduce boilerplate by adding 'actions' from named import at top
 VisibleTodoList = withRouter(connect(
   mapStateToProps,
-  { onTodoClick: toggleTodo }
+  actions
 )(VisibleTodoList));
 
 export default VisibleTodoList;
